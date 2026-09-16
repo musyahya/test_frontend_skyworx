5d6c673e87adc37a75c51c966cbeecd1e9bae413
musyahya <mananabdul37654@gmail.com>
2026-09-17 04:08:00 +0700

    pasang token di axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,24 +1,19 @@
 import Axios from "axios";
-// import { getAuthCookie } from "./cookie";
-
-// const isServer = typeof window === "undefined";
+import { getTokenCookie } from "./cookie";
 
 const axios = Axios.create({
   baseURL:  process.env.BASE_URL_ORIGIN,
 });
 
-/**
- * Interceptor Khusus Server
- */
+
 axios.interceptors.request.use(
   async (config) => {
     try {
-      // Di Server Action, kita bisa langsung panggil fungsi cookie server
-    //   const authData = await getAuthCookie();
+      const token = await getTokenCookie();
 
-    //   if (authData?.token) {
-    //     config.headers.Authorization = `Bearer ${authData.token}`;
-    //   }
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
 
     } catch (error) {
       console.error("❌ [SERVER-AXIOS] Interceptor Error:", error);
