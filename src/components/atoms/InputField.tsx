83fb0bd0edc12c19592a5b7ff303788edf1259ed
musyahya <mananabdul37654@gmail.com>
2026-09-16 14:54:39 +0700

    is show password

--- src/components/atoms/InputField.tsx
+++ src/components/atoms/InputField.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import cn from "@/src/lib/cn";
-import React, { forwardRef, InputHTMLAttributes, ReactNode, useId } from "react";
+import { Eye, EyeClosed } from "lucide-react";
+import React, { forwardRef, InputHTMLAttributes, ReactNode, useId, useState } from "react";
 
 export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -17,11 +18,13 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
       error,
       leftIcon,
       rightIcon,
+      type = "text",
       ...props
     },
     ref,
   ) => {
     const id = useId();
+    const [isShowPassword, setIsShowPassword] = useState(false)
 
     return (
       <div className="flex flex-col gap-2">
@@ -29,20 +32,41 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
           {label}
         </label>
 
-        <input
-          ref={ref}
-          id={id}
-          suppressHydrationWarning
-          {...props}
-          className={cn(
-            "block w-full rounded-xl border border-slate-800  px-4 py-3 bg-slate-950 text-sm transition-colors focus:outline-none focus:ring-1",
-            !!error ? [
-                "placeholder-red-100/50 focus:border-red-400 focus:ring-indigo-400"
-            ] : [
-                "text-slate-100 placeholder-slate-600 focus:border-indigo-500 focus:ring-indigo-500"
-            ]
-          )}
-        />
+        <div className="relative">
+            <input
+            ref={ref}
+            id={id}
+            type={isShowPassword ? "text" : type}
+            suppressHydrationWarning
+            {...props}
+            className={cn(
+                "block w-full rounded-xl border border-slate-800  px-4 py-3 bg-slate-950 text-sm transition-colors focus:outline-none focus:ring-1",
+                !!error ? [
+                    "placeholder-red-100/50 focus:border-red-400 focus:ring-indigo-400"
+                ] : [
+                    "text-slate-100 placeholder-slate-600 focus:border-indigo-500 focus:ring-indigo-500"
+                ]
+            )}
+            />
+
+            <div className="absolute right-4 top-1/2 -translate-y-1/2 flex items-center gap-2">
+                {type === "password" && (
+                    <button
+                        type="button"
+                        tabIndex={-1}
+                        onClick={() => setIsShowPassword(!isShowPassword)}
+                        className="text-neutral-main hover:text-isme-main transition-colors outline-none"
+                    >
+                        {isShowPassword ? (
+                        <Eye size={14} />
+                        ) : (
+                        <EyeClosed size={14} />
+                        )}
+                    </button>
+                    )
+                }
+            </div>
+        </div>
 
         {!!error && (
             <p
