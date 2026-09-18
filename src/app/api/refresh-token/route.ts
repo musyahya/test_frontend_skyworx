6ebb1da15c69f2131f1aadaf5ee72e70b54b8cd9
musyahya <mananabdul37654@gmail.com>
2026-09-18 07:14:10 +0700

    axios interceptors refresh token

---
+++ src/app/api/refresh-token/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { refresh_token } = body;
+
+    if (!refresh_token) {
+      return NextResponse.json(
+        { message: "Refresh token must required" },
+        { status: 422 }
+      );
+    }
+
+    const token = {
+      access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30",
+      refresh_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gYXNkIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.q_BDT_ruTKPRjNlUkjLWQFUFGtESAXMh4H0ToUuCFVg",
+    };
+
+    return NextResponse.json(
+      token,
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,12 @@
 import Axios from "axios";
-import { getTokenCookie } from "./cookie";
+import { getTokenCookie, setTokenCookie } from "./cookie";
 
 const axios = Axios.create({
   baseURL:  process.env.BASE_URL_ORIGIN,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 
 axios.interceptors.request.use(
   async (config) => {
@@ -17,10 +19,51 @@ axios.interceptors.request.use(
 
     } catch (error) {
       console.error("❌ [SERVER-AXIOS] Interceptor Error:", error);
+      return Promise.reject(error);
     }
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const token = await getTokenCookie();
+        if (!token?.refresh_token) {
+          throw new Error("Refresh token not found.");
+        }
+
+        const response = await Axios.post(
+          `${process.env.BASE_URL_ORIGIN}/refresh-token`,
+          {
+            refresh_token: token.refresh_token,
+          }
+        );
+
+        console.log("response", response)
+
+        await setTokenCookie({
+          access_token: response.data.access_token,
+          refresh_token: response.data.refresh_token || token.refresh_token,
+        });
+
+        originalRequest.headers.Authorization = `Bearer ${response.data.access_token}`;
+
+        return axios(originalRequest);
+      } catch (refreshError) {        
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default axios;
